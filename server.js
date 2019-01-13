e96ce2576c57c3a86b1c37c0b6403b2e25f97ac2
Carmen Perezchica <crperezchica@gmail.com>
2019-01-12 22:14:53 -0800

    router passing requested pathname

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 //we are able to make sure of node.js http module by requiring it and assiging the result of the require to a local variable
 //making our local variable an object that carries all the methods the "http" module provides
 var http = require("http");
+var url = require("url"); // created to distinguish requests based on the URL path requested
 
 //createServer is a function call that http module offers
 //function returns an object and this object has a method named .listen
@@ -18,12 +19,18 @@ var http = require("http");
 // var server = http.createServer();
 // server.listen(8888);
 
-//when callback fires and onRequest() function gets triggered two parameters passed into it: request and response
-//request and response are objects 
-//-> use their methods to handle the details of the HTTP request and respond to request
+//created start function to export the function that starts the server
+//allowing to start our HTTP in index.js
 function start() {
+    //when callback fires and onRequest() function gets triggered two parameters passed into it: request and response
+    //request and response are objects 
+    //-> use their methods to handle the details of the HTTP request and respond to request
     function onRequest(request, response) {
-        console.log("Request recieved.");
+        var pathname = url.parse(request.url).pathname;
+        console.log("Request for " + pathname + " received.");
+
+        route(pathname);
+
         response.writeHead(200, {"Content-Type": "text/plain"}); //when request recieved sends an HTTP status and content-type in the HTTP response header
         response.write("Hello World"); // function sends text in the HTTP respone body
         response.end();//to finish response
