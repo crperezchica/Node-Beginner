b0005b73d19bdcf7648a426341c350a5e3ffcb34
Carmen Perezchica <crperezchica@gmail.com>
2019-01-12 21:26:09 -0800

    two different ways to start server and my comments

--- helloworld.js
+++ helloworld.js
@@ -1,1 +1,13 @@
 console.log("Hello World");
+
+//Since Node.js is single threaded this would slow our app querying the database
+//Synchronous: 1st-query the database AFTER DONE 2nd- write to console
+// var result = database.query("SELECT * FROM hugetable");
+// console.log("Hello World");
+
+// //To prevent above, instead of expecting database query to return a result
+// //we pass an anonymous function as a second parameter
+// database.query("SELECT * FROM hugetable", function(rows) {
+//   var result = rows;
+// })
+// console.log("Hello World");
--- server.js
+++ server.js
@@ -1,7 +1,30 @@
+//requires http module thru node.js and makes it avaiable through variable http
 var http = require("http");
 
+//createServer is a function call that http module offers
+//function returns an object and this object has a method named .listen
+//passing an anonymous function
 http.createServer(function(request, response){
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("Hello World");
     response.end();
-}).listen(8888);
+}).listen(8888); //.listen(portnumber) HTTP server is going to listen on
+
+//Another option to start server 
+// var http = require("http");
+
+// var server = http.createServer();
+// server.listen(8888);
+
+//when callback fires and onRequest() function gets triggered two parameters passed into it: request and response
+//request and response are objects 
+//-> use their methods to handle the details of the HTTP request and respond to request
+// function onRequest(request,response) {
+//     console.log("Request recieved.");
+//     response.writeHead(200, {"Content-Type": "text/plain"}); //when request recieved sends an HTTP status and content-type in the HTTP response header
+//     response.write("Hello World"); // function sends text in the HTTP respone body
+//     response.end();//to finish response
+// }
+
+// http.createServer(onRequest).listen(5000);
+// console.log("server has started");
