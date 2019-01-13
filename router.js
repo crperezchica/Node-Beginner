ba861d9fda39504cd3c3e85286778f4004e5d5ed
Carmen Perezchica <crperezchica@gmail.com>
2019-01-13 14:59:45 -0800

    done - simple node.js web app

--- router.js
+++ router.js
@@ -1,12 +1,12 @@
 
 //Checking to see if request handler for the given pathname exists, if it does it calls the according function
-function route(handle, pathname, response, postData) {
+function route(handle, pathname, response, request) {
   console.log("About to route a request for " + pathname);
   if (typeof handle[pathname] === 'function') {
-    handle[pathname](response, postData);
+    handle[pathname](response, request);
   } else {
     console.log("No request handler found for " + pathname);
-    response.writeHead(404, {"Content-Type": "text/plain"});
+    response.writeHead(404, {"Content-Type": "text/html"});
     response.write("404 Not Found");
     response.end(); 
   }
--- server.js
+++ server.js
@@ -1,30 +1,67 @@
 
+// var formidable = require('formidable'),
 var http = require('http');
+    // sys = require('sys');
 var url = require('url'); // created to distinguish requests based on the URL path requested
 
 function start(route, handle) {
     function onRequest(request, response) {
-        var postData = "";
         var pathname = url.parse(request.url).pathname;
         console.log("Request for " + pathname + " received.");
-
-        request.setEncoding("utf8"); //expecting encoding of received data to be utf8
-
-        //called when a new chunk of data was received 
-        request.addListener("data", function(postDataChunk) {
-            postData += postDataChunk;
-            console.log("Recieved POST data chunk ' "+
-            postDataChunk + "'. ");
-        });
-
-        //called when all chunks of data have been recieved
-        request.addListener("end", function() {
-            route(handle, pathname, response, postData);
-        });
+        route(handle, pathname, response, request);
     }
     http.createServer(onRequest).listen(8888);
-    console.log("Server has started");
+    console.log("Server has started.");
 }
+
+// http.createServer(function(req,res) {
+//     if (req.url == '/upload' && req.method.toLowerCase() == 'post') {
+//         //parse a file upload
+//         var form = new formidable.IncomingForm();
+//         form.parse(req, function(error, fields, files) {
+//             res.writeHead(200, {'content-type': 'text/plain'});
+//             res.write('recieved upload:\n\n');
+//             res.end(sys.inspect({fields: fields, files: files}));
+//         });
+//         return;
+//     }
+//     //show a file upload form
+//     res.writeHead(200, {'content-type': 'text/html'});
+//     res.end(
+//         '<form action="/upload" enctype="multipart/form-data" '+
+//         'method="post">'+
+//         '<input type="text" name="title"><br>'+
+//         '<input type="file" name="upload" multiple="multiple"><br> '+
+//         '<input type="submit" value="Upload">'+
+//         '</form>'
+//     );
+// }).listen(8888);
+
+
+
+// // function start(route, handle) {
+// //     function onRequest(request, response) {
+// //         var postData = "";
+// //         var pathname = url.parse(request.url).pathname;
+// //         console.log("Request for " + pathname + " received.");
+
+// //         request.setEncoding("utf8"); //expecting encoding of received data to be utf8
+
+// //         //called when a new chunk of data was received 
+// //         request.addListener("data", function(postDataChunk) {
+// //             postData += postDataChunk;
+// //             console.log("Recieved POST data chunk ' "+
+// //             postDataChunk + "'. ");
+// //         });
+
+// //         //called when all chunks of data have been recieved
+// //         request.addListener("end", function() {
+// //             route(handle, pathname, response, postData);
+// //         });
+// //     }
+// //     http.createServer(onRequest).listen(8888);
+// //     console.log("Server has started");
+// // }
 
 exports.start = start;
 
