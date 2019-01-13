88adcce26d4951d4df579548ed67694a510120e7
Carmen Perezchica <crperezchica@gmail.com>
2019-01-12 21:47:04 -0800

    turned server.js script into a module that starts server through index.js

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 //requires http module thru node.js and makes it avaiable through variable http
+//we are able to make sure of node.js http module by requiring it and assiging the result of the require to a local variable
+//making our local variable an object that carries all the methods the "http" module provides
 var http = require("http");
 
 //createServer is a function call that http module offers
 //function returns an object and this object has a method named .listen
 //passing an anonymous function
-http.createServer(function(request, response){
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World");
-    response.end();
-}).listen(8888); //.listen(portnumber) HTTP server is going to listen on
+// http.createServer(function(request, response){
+//     response.writeHead(200, {"Content-Type": "text/plain"});
+//     response.write("Hello World");
+//     response.end();
+// }).listen(8888); //.listen(portnumber) HTTP server is going to listen on
 
 //Another option to start server 
 // var http = require("http");
@@ -19,12 +21,15 @@ http.createServer(function(request, response){
 //when callback fires and onRequest() function gets triggered two parameters passed into it: request and response
 //request and response are objects 
 //-> use their methods to handle the details of the HTTP request and respond to request
-// function onRequest(request,response) {
-//     console.log("Request recieved.");
-//     response.writeHead(200, {"Content-Type": "text/plain"}); //when request recieved sends an HTTP status and content-type in the HTTP response header
-//     response.write("Hello World"); // function sends text in the HTTP respone body
-//     response.end();//to finish response
-// }
+function start() {
+    function onRequest(request, response) {
+        console.log("Request recieved.");
+        response.writeHead(200, {"Content-Type": "text/plain"}); //when request recieved sends an HTTP status and content-type in the HTTP response header
+        response.write("Hello World"); // function sends text in the HTTP respone body
+        response.end();//to finish response
+    }
+    http.createServer(onRequest).listen(8888);
+    console.log("Server has started");
+}
 
-// http.createServer(onRequest).listen(5000);
-// console.log("server has started");
+exports.start = start;
